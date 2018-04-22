d867cf7bb6973e6a1ff2cd3074b2543dcf539241
Jucong <hejucong1995@gmail.com>
2018-04-21 22:25:50 -0400

    matrix working with MQTT

--- finalProject.ino
+++ finalProject.ino
@@ -1,20 +1,37 @@
 #include <WiFiEsp.h>
 #include <WiFiEspClient.h>
 #include <PubSubClient.h>
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
 
 #define ADAFRUIT_USERNAME  "Jucong"
 #define AIO_KEY  "01a85723468e409bb15488a77efe724c"
-#define SUB_FEED_PATH ADAFRUIT_USERNAME "/feeds/test"
-#define PUB_FEED_PATH ADAFRUIT_USERNAME "/feeds/testPub"
+#define SLIDER_PATH ADAFRUIT_USERNAME "/feeds/slider"
+#define PUB_FEED_PATH ADAFRUIT_USERNAME "/feeds/publish"
+
+#define CLK 11  // MUST be on PORTB! (Use pin 11 on Mega)
+#define LAT A3
+#define OE  9
+#define A   A0
+#define B   A1
+#define C   A2
 
 const char* ssid = "Jucong";
 const char* password =  "Hjc1995ma";
 int status = WL_IDLE_STATUS; // the Wifi radio's status
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.println(topic);
-  Serial.write(payload, length);
-  Serial.println("");
+  matrix.fillScreen(0);
+  matrix.setCursor(0, 1);
+  char s[length + 1];
+  Serial.println(length);
+  for (int i = 0; i < length; i++) {
+    s[i] = (char)payload[i];
+  }
+  s[length] = '\0';
+  matrix.print(s);
+  matrix.swapBuffers(false);
 }
 
 WiFiEspClient espClient;
@@ -27,7 +44,7 @@ void reconnect() {
     if (client.connect("MEGA", ADAFRUIT_USERNAME, AIO_KEY )) {
  
       Serial.println("connected and Subscribing");  
-      client.subscribe(SUB_FEED_PATH, 1);
+      client.subscribe(SLIDER_PATH, 1);
       client.publish(PUB_FEED_PATH, "Connect");
       
     } else {
@@ -58,6 +75,9 @@ void setup() {
     status = WiFi.begin(ssid, password);
   }
   Serial.println("Connected to the WiFi network");
+  matrix.begin();
+  matrix.setTextWrap(false); // Allow text to run off right edge
+  matrix.setTextSize(2);
  
 }
 
