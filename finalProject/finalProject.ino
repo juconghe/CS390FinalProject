0c12266607dd44ccae4b3947e5c1e0307fa24278
juconghe <hejucong1995@gmail.com>
2018-04-23 01:14:50 -0400

    move NTP to header file and fix udp problem

--- finalProject/finalProject.ino
+++ finalProject/finalProject.ino
@@ -3,13 +3,14 @@
 #include <PubSubClient.h>
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <RGBmatrixPanel.h> // Hardware-specific library
-#include "WiFiEspUdp.h"
 #include <TimeLib.h>
+#include "NTP.h"
 
 #define ADAFRUIT_USERNAME  "Jucong"
 #define AIO_KEY  "01a85723468e409bb15488a77efe724c"
 #define SLIDER_PATH ADAFRUIT_USERNAME "/feeds/slider"
 #define PUB_FEED_PATH ADAFRUIT_USERNAME "/feeds/publish"
+#define F2(progmem_ptr) (const __FlashStringHelper *)progmem_ptr
 
 #define CLK 11  // MUST be on PORTB! (Use pin 11 on Mega)
 #define LAT A3
@@ -18,30 +19,25 @@
 #define B   A1
 #define C   A2
 
-const char* ssid = "AHHA Lab Wifi";
-const char* password =  "ramanujan";
+const char* ssid = "weihai";
+const char* password =  "weihaihenshuai";
 int status = WL_IDLE_STATUS; // the Wifi radio's status
-WiFiEspUDP Udp;
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
-IPAddress timeServer(132, 163, 97, 3);
-const int timeZone = -4;  // Eastern Daylight Time (USA)
-unsigned int localPort = 8888;  // local port to listen for UDP packets
 bool on = true;
+String mode = "0";
 
-time_t getNtpTime();
-void sendNTPpacket(IPAddress &address);
-
+const char str[] PROGMEM = "Go to stop&shop buy some juice";
+int    textX   = matrix.width(),
+       textMin = sizeof(str) * -6;
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  matrix.fillScreen(0);
-  matrix.setCursor(0, 1);
   char s[length + 1];
   for (int i = 0; i < length; i++) {
     s[i] = (char)payload[i];
   }
   s[length] = '\0';
-  matrix.print(s);
-  matrix.swapBuffers(false);
+  mode = String(s);
+  Serial.println(mode);
 }
 
 WiFiEspClient espClient;
@@ -49,16 +45,12 @@ PubSubClient client("io.adafruit.com", 1883, callback, espClient);
 
 void reconnect() {
   while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-
+    Serial.println(F("Connecting to MQTT..."));
     if (client.connect("MEGA", ADAFRUIT_USERNAME, AIO_KEY )) {
-
-      Serial.println("connected and Subscribing");
       client.subscribe(SLIDER_PATH, 1);
-      client.publish(PUB_FEED_PATH, "Connect");
-
+      Serial.println(F("Connected"));
     } else {
-      Serial.print("failed with state ");
+      Serial.print(F("failed with state "));
       Serial.print(client.state());
       delay(2000);
 
@@ -153,11 +145,20 @@ void drawClock() {
     } else {
        matrix.drawPixel(5, 3, matrix.Color333(9, 9, 9));
        matrix.drawPixel(5, 6, matrix.Color333(9, 9, 9));
-    } 
+    }
   }
   on = !on;
- }
+}
 
+void scrollText() {
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false); // Allow text to run off right edge
+  matrix.setTextColor(matrix.Color333(0, 4, 0));
+  matrix.setCursor(textX, 8);
+  matrix.print(F2(str));
+  if((--textX) < textMin) textX = matrix.width();
+
+}
 void setup() {
 
   Serial.begin(115200);
@@ -175,15 +176,20 @@ void setup() {
     // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, password);
   }
-  Serial.println("Connected to the WiFi network");
+  Serial.println(F("Connected to the WiFi network"));
 
   Udp.begin(localPort);
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
+  time_t syncTime = getNtpTime();
+  while (syncTime == 0) {
+    Serial.println(F("Sycning time"));
+    syncTime = getNtpTime();
+    delay(1000);
+  }
+  setTime(syncTime);
 
   matrix.begin();
-  matrix.setTextWrap(false); // Allow text to run off right edge
   matrix.setTextSize(1);
+
 }
 
 
@@ -194,62 +200,10 @@ void loop() {
   }
   matrix.fillScreen(matrix.Color333(0, 0, 0));
   drawClock();
-  drawSun();
+//  drawSun();
+  scrollText();
   matrix.swapBuffers(false);
   delay(100);
   client.loop();
 
-}
-
-
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  sendNTPpacket(timeServer);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
 }
